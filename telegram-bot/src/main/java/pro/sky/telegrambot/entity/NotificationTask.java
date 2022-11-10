a38bce5bbe0c353754c4e86d05338ee1a8b570d6
Hyp <otlichnik56@yandex.ru>
2022-11-10 23:20:38 +0500

    Initial commit

---
+++ telegram-bot/src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
@@ -0,0 +1,50 @@
+package pro.sky.telegrambot.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notification_task")
+public class NotificationTask {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String chatId;
+    private String message;
+    private LocalDateTime dateTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime localDateTime) {
+        this.dateTime = localDateTime;
+    }
+}
---
+++ telegram-bot/src/main/java/pro/sky/telegrambot/liquibase/table-work.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+
+-- changeset nurkatovich:1
+CREATE TABLE notification_task(
+    id          INTEGER PRIMARY KEY,
+    chatId      TEXT,
+    message     TEXT,
+    dateTime    TIMESTAMP
+);
